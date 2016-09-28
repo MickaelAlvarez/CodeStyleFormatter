fb1ada73f8401d6a2b7e087b63ae6dfe69543946
mickmouette <mickmouette@mickmouette-W51XTU>
2016-09-28 23:21:17 +0200

    Ajout du deuxième paramétre de la version ligne de commande
    Ajout de fichier de tests
    > Erreur à corriger

--- src/formatter/StyleFormatter.java
+++ src/formatter/StyleFormatter.java
@@ -1,5 +1,10 @@
 package formatter;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.eclipse.jdt.core.ToolFactory;
@@ -12,26 +17,47 @@ import org.eclipse.text.edits.TextEdit;
 
 public class StyleFormatter {
 	private CodeFormatter codeFormatter; // TODO On peut le rendre static
-	
+
 	public StyleFormatter(Map<String, String> options) {
 		this.codeFormatter = ToolFactory.createCodeFormatter(options);
 	}
-	
-	public IDocument apply(String source) throws MalformedTreeException, BadLocationException {
+
+	public IDocument apply(String filePath) throws MalformedTreeException, BadLocationException {
+		String source = getSourceFile(filePath);
 		IDocument document = new Document(source);
 		TextEdit edit = getFormatter(source);
 		edit.apply(document);
 		return document;
 	}
-	
+
 	private TextEdit getFormatter(String source) {
-		return codeFormatter.format(
-		        CodeFormatter.K_COMPILATION_UNIT, // format a compilation unit
-		        source, // source to format
-		        0, // starting position
-		        source.length(), // length
-		        0, // initial indentation
-		        System.getProperty("line.separator") // line separator
-		        );
+		return codeFormatter.format(CodeFormatter.K_COMPILATION_UNIT, // format a compilation unit
+				source, // source to format
+				0, // starting position
+				source.length(), // length
+				0, // initial indentation
+				System.getProperty("line.separator") // line separator
+		);
+	}
+
+	private String getSourceFile(String filePath) {
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,36 +1,43 @@
 package main;
 
 import java.io.File;
+import java.util.List;
 
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.text.edits.MalformedTreeException;
 
 import formatter.StyleFormatter;
+import parser.ListFileNameParser;
 import parser.OptionFileParser;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String parserOptionPath;
-		File parserOptionFile;
+		List<String> filesToFormatPaths;
+		String parserOptionPath, filesToFormatPath;
+		File parserOptionFile, filesToFormatFile;
 		
 //		parserOptionPath = args[0]; // Used in command line
-		parserOptionPath = "test.xml";
+//		filesToFormatPath = args[1]; // Used in command line
+		parserOptionPath = "test.xml"; // Pour tester
+		filesToFormatPath = "listFile"; // Pour tester
 		
-		System.out.println("Fichier : " + parserOptionPath);
-
 		parserOptionFile = new File(parserOptionPath);
+		filesToFormatFile = new File(filesToFormatPath);
 
-		if (parserOptionFile.exists() && parserOptionFile.isFile()) {
-			String source = "public enum X { A,B,C,D,E,F}; private static String pouette = null;";
+		if (parserOptionFile.exists() && parserOptionFile.isFile() && filesToFormatFile.exists() && filesToFormatFile.isFile()) {
+			filesToFormatPaths = ListFileNameParser.parse(filesToFormatFile);
 			OptionFileParser parser = new OptionFileParser(parserOptionPath);
 			StyleFormatter formatter = new StyleFormatter(parser.parse());
 
-			try {
-				System.out.println(formatter.apply(source).get());
-			} catch (MalformedTreeException | BadLocationException e) {
-				e.printStackTrace();
+			for(String filePath : filesToFormatPaths) {
+				try {
+					System.out.println(formatter.apply(filePath).get());
+				} catch (MalformedTreeException | BadLocationException e) {
+					e.printStackTrace();
+				}
 			}
+			
 		}
 	}
 
