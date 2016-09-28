7b954ff61b122128a74a96dec592c5991a6446f1
mickmouette <mickmouette@mickmouette-W51XTU>
2016-09-28 20:47:47 +0200

    Ajout d'un argument pour traiter le fichier xml d'options je formattage

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.File;
+
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.text.edits.MalformedTreeException;
 
@@ -7,17 +9,28 @@ import formatter.StyleFormatter;
 import parser.OptionFileParser;
 
 public class Main {
-	// Purge equinox
 
 	public static void main(String[] args) {
-		String source = "public enum X { A,B,C,D,E,F}; private static String pouette = null;";
-		OptionFileParser parser = new OptionFileParser("test.xml");
-		StyleFormatter formatter = new StyleFormatter(parser.parse());
+		String parserOptionPath;
+		File parserOptionFile;
 		
-		try {
-			System.out.println(formatter.apply(source).get());
-		} catch (MalformedTreeException | BadLocationException e) {
-			e.printStackTrace();
+//		parserOptionPath = args[0]; // Used in command line
+		parserOptionPath = "test.xml";
+		
+		System.out.println("Fichier : " + parserOptionPath);
+
+		parserOptionFile = new File(parserOptionPath);
+
+		if (parserOptionFile.exists() && parserOptionFile.isFile()) {
+			String source = "public enum X { A,B,C,D,E,F}; private static String pouette = null;";
+			OptionFileParser parser = new OptionFileParser(parserOptionPath);
+			StyleFormatter formatter = new StyleFormatter(parser.parse());
+
+			try {
+				System.out.println(formatter.apply(source).get());
+			} catch (MalformedTreeException | BadLocationException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
